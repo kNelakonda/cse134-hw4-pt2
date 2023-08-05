d2728441eaaf54760c460f0c6f7fd617ae367201
kNelakonda <knelakon@ucsd.edu>
2023-08-05 09:22:22 -0700

    Defined the custom element

--- projectCard.js
+++ projectCard.js
@@ -2,13 +2,14 @@ function init() {
 console.log("hello World");
 }
 
-
 class ProjectCard extends HTMLElement{
     constructor(){
         super();
-        this.attachShadow();
+        this.attachShadow({mode: "open"});
+        this.shadowRoot.innerHTML = "<h2>New web component, a project-card<h2>";
     }
 }
 
 
 window.addEventListener('DOMContentLoaded', init);
+window.customElements.define("project-card", ProjectCard);
