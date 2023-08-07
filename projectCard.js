c3ed608e785de8dcb622e95073b782e30a942b38
kNelakonda <knelakon@ucsd.edu>
2023-08-07 00:28:30 -0700

    added another project, changed reload

--- projectCard.js
+++ projectCard.js
@@ -9,60 +9,73 @@ function init() {
     
 }
 
-function createLocalData(title, image, alt, description, link){
-    let myProject = {
-        "project-title": "Project Title",
-        "project-img": "./assets/ece196project.png",
-        "project-img-alt": "Image of the Project",
-        "project-desc": "Project description",
-        "project-link": "Project link"
-    };
+function createLocalData(){
 
-    if(title){
-        console.log("there is a title");
-        myProject["project-title"] = title; 
-    }
-    if(image){
-        myProject["project-img"] = image; 
-    }
-    if(alt){
-        myProject["project-img-alt"] = alt; 
-    }
-    if(description){
-        myProject["project-desc"] = description; 
-    }
-    if(link){
-        myProject["project-link"] = link; 
-    }
-    myProject = {
-        "project-title": "Workout Buddy",
+    /*let projects = [];
+    let myProject1 = {"project-title": "Workout Buddy", 
         "project-img": "./assets/ece196project.png",
         "project-img-alt": "Image of my ECE 196 Project",
         "project-desc": "This is a wearable device that connects to an iOS application that will count your reps for specific workouts, as well as the quality of the reps, and display it on the app.",
         "project-link": "https://github.com/gman-ui/ECE196Project/blob/main/index.md"
-    }
-    return myProject;
+    };
+
+    let myProject2 = {
+        "project-title": "Tasseography",
+        "project-img": "./assets/cse110project.png",
+        "project-img-alt": "Image of my CSE 110 Project",
+        "project-desc": "This is a project that is meant to simulate tea leave fortune telling, or Tasseography. You go through the actions of preparing and drinking tea, and based on the shape of the tea leaves at the bottom, you receive a fortune",
+        "project-link": "https://github.com/cse110-sp23-group18/cse110-sp23-group18/blob/prod/README.md"
+    };
+
+    projects.push(JSON.stringify(myProject1));
+    console.log("THIS IS myPROJECT1 " + projects[0]);
+    console.log("THE TYPE OF OF myPROJECT1 " + typeof(projects[0]));
+    console.log("NOW I PARSE myPROJECT1 INTO: " + JSON.parse(projects[0]));
+
+    projects.push((myProject2));
+    */
+    let projects = {
+        
+        'project1' : {
+            'project-title': 'Workout Buddy',
+            'project-img': './assets/ece196project.png',
+            'project-img-alt': 'Image of my ECE 196 Project',
+            'project-desc': 'This is a wearable device that connects to an iOS application that will count your reps for specific workouts, as well as the quality of the reps, and display it on the app.',
+            'project-link': 'https://github.com/gman-ui/ECE196Project/blob/main/index.md'
+        },
+        'project2' : {
+            'project-title': 'Tasseography',
+            'project-img': './assets/cse110project.png',
+            'project-img-alt': "Image of my CSE 110 Project",
+            'project-desc': 'This is a project that is meant to simulate tea leave fortune telling, or Tasseography. You go through the actions of preparing and drinking tea, and based on the shape of the tea leaves at the bottom, you receive a fortune',
+            'project-link': 'https://github.com/cse110-sp23-group18/cse110-sp23-group18/blob/prod/README.md'
+        }
+    };
+    
+    console.log(Object.keys(projects));
+    return JSON.stringify(projects);
 }
 
 
 function saveLocal(){
-    const project = createLocalData();
-    const projectString = JSON.stringify(project);
-    localStorage.setItem("project", projectString);
+    const projects = createLocalData();
+    //const projectString = JSON.stringify(project);
+    localStorage.setItem("projects", projects);
     document.getElementById("local-button").disabled= false;
 }
 
 class ProjectCard extends HTMLElement{
-    constructor(){
+    constructor(projectParts){
         super();
         this.attachShadow({mode: "open"});
-
+        /*
         let projectParts;
-        if(localStorage.getItem("project")){
+        if(localStorage.getItem("projects")){
            projectParts = JSON.parse(localStorage.getItem("project"));
         } else {
             projectParts = JSON.parse(getRemote());
         }
+        */
          
         let cardHolder = document.createElement("article");
         this.shadowRoot.appendChild(cardHolder)
@@ -90,6 +103,11 @@ class ProjectCard extends HTMLElement{
             border: 1px solid black;
             padding: 1%;
             margin-top: 1%;
+        }
+
+        article > img {
+            height: 100%;
+            width: 100%;
         }
         `;
         this.shadowRoot.appendChild(styleSheet);
@@ -126,9 +144,29 @@ async function getRemote(){
 
 
 function showProject(){
-    let card = document.createElement("project-card");
     let body = document.getElementsByTagName('main')[0];
-    body.appendChild(card);
+    body.innerHTML = "";
+    let projectParts;
+    let projects = localStorage.getItem("projects");
+    projects = JSON.parse(projects);
+    let keys = Object.keys(projects);
+    console.log(keys);
+    //projects = JSON.parse(projects);
+    if(projects){
+        console.log(projects.length);
+        for(let i = 0; i < keys.length; i++){
+            let project = projects[keys[i]];
+            console.log(typeof(projects[keys[i]]));
+            let card = new ProjectCard(project);
+            body.appendChild(card);
+
+        }
+       
+    } else {
+        projectParts = JSON.parse(getRemote());
+    }
+
+   
 }
 
 
