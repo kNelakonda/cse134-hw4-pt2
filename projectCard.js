86dcda2bbc73dc7ef221c5ba7ff1d1e7e9ca5188
kNelakonda <knelakon@ucsd.edu>
2023-08-07 01:22:27 -0700

    fixed remote reach

--- projectCard.js
+++ projectCard.js
@@ -3,39 +3,19 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
     saveLocal();
-    getRemote().then( 
-        document.getElementById("remote-button").disabled= false
+    getRemote().then( result =>{
+        document.getElementById("remote-button").disabled= false;
+        document.getElementById("remote-button").addEventListener("click", function (){
+            showRemoteProject(result);
+        });
+
+    }
     );
     
 }
 
 function createLocalData(){
-
-    /*let projects = [];
-    let myProject1 = {"project-title": "Workout Buddy", 
-        "project-img": "./assets/ece196project.png",
-        "project-img-alt": "Image of my ECE 196 Project",
-        "project-desc": "This is a wearable device that connects to an iOS application that will count your reps for specific workouts, as well as the quality of the reps, and display it on the app.",
-        "project-link": "https://github.com/gman-ui/ECE196Project/blob/main/index.md"
-    };
-
-    let myProject2 = {
-        "project-title": "Tasseography",
-        "project-img": "./assets/cse110project.png",
-        "project-img-alt": "Image of my CSE 110 Project",
-        "project-desc": "This is a project that is meant to simulate tea leave fortune telling, or Tasseography. You go through the actions of preparing and drinking tea, and based on the shape of the tea leaves at the bottom, you receive a fortune",
-        "project-link": "https://github.com/cse110-sp23-group18/cse110-sp23-group18/blob/prod/README.md"
-    };
-
-    projects.push(JSON.stringify(myProject1));
-    console.log("THIS IS myPROJECT1 " + projects[0]);
-    console.log("THE TYPE OF OF myPROJECT1 " + typeof(projects[0]));
-    console.log("NOW I PARSE myPROJECT1 INTO: " + JSON.parse(projects[0]));
-
-    projects.push((myProject2));
-    */
     let projects = {
-        
         'project1' : {
             'project-title': 'Workout Buddy',
             'project-img': './assets/ece196project.png',
@@ -51,15 +31,12 @@ function createLocalData(){
             'project-link': 'https://github.com/cse110-sp23-group18/cse110-sp23-group18/blob/prod/README.md'
         }
     };
-    
-    console.log(Object.keys(projects));
     return JSON.stringify(projects);
 }
 
 
 function saveLocal(){
     const projects = createLocalData();
-    //const projectString = JSON.stringify(project);
     localStorage.setItem("projects", projects);
     document.getElementById("local-button").disabled= false;
 }
@@ -68,14 +45,6 @@ class ProjectCard extends HTMLElement{
     constructor(projectParts){
         super();
         this.attachShadow({mode: "open"});
-        /*
-        let projectParts;
-        if(localStorage.getItem("projects")){
-           projectParts = JSON.parse(localStorage.getItem("project"));
-        } else {
-            projectParts = JSON.parse(getRemote());
-        }
-        */
          
         let cardHolder = document.createElement("article");
         this.shadowRoot.appendChild(cardHolder)
@@ -116,7 +85,7 @@ class ProjectCard extends HTMLElement{
 }
 
 async function getRemote(){
-    const binID = "64cf310e9d312622a38cae1f";
+    const binID = "64d09f059d312622a38d2c9b";
     const key = "$2b$10$J/ZtAuCvdOmlswtSwYsTj.ubRUwEshZ4KJ/0pmKQC/oJKjkcF00ee";
     const root = "https://api.jsonbin.io/v3/b/";
     let data;
@@ -133,11 +102,10 @@ async function getRemote(){
     } catch(error){
         console.error("An error fetching data. Error: " + error);
     }
-    
-    console.log(data.record);
     if (!data.record) {
         throw Error("Did not receive data");
     }
+    console.log(data.record);
     return data.record;
 
 }
@@ -146,12 +114,10 @@ async function getRemote(){
 function showProject(){
     let body = document.getElementsByTagName('main')[0];
     body.innerHTML = "";
-    let projectParts;
     let projects = localStorage.getItem("projects");
     projects = JSON.parse(projects);
     let keys = Object.keys(projects);
     console.log(keys);
-    //projects = JSON.parse(projects);
     if(projects){
         console.log(projects.length);
         for(let i = 0; i < keys.length; i++){
@@ -161,12 +127,29 @@ function showProject(){
             body.appendChild(card);
 
         }
-       
     } else {
-        projectParts = JSON.parse(getRemote());
+        body.innerHTML="<strong>Unable to fetch the projects locally, maybe try remotely?</strong>";
     }
+}
 
-   
+function showRemoteProject(result) {
+    console.log(result);
+    let body = document.getElementsByTagName('main')[0];
+    body.innerHTML = "";
+    let keys = Object.keys(result);
+    console.log(keys);
+    if(result){
+        console.log(result.length);
+        for(let i = 0; i < keys.length; i++){
+            let project = result[keys[i]];
+            console.log(typeof(result[keys[i]]));
+            let card = new ProjectCard(project);
+            body.appendChild(card);
+
+        }
+    } else {
+        body.innerHTML = "<strong>Unable to fetch the projects from the remote location, maybe try locally?</strong>";
+    }
 }
 
 
